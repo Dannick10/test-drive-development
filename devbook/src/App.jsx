1765052aaa7b64f88786d7cb921698fbabcaba6b
Dannick010 <danmaggot2@gmail.com>
2025-06-04 14:48:52 -0300

    tdd com react / instalando materialui

---
+++ devbook/src/App.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography data-test="heading" variant="h2" component="h2">
+        Olá Dev
+      </Typography>
+    </>
+  );
+}
+
+export default App;
